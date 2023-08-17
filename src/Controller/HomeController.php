0f8596239638e443d62b514ba2dd9abc1eb9975d
nhathoa2002 <142466074+nhathoa2002@users.noreply.github.com>
2023-08-17 09:19:02 +0700

    Add files via upload

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -9,9 +9,9 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     #[Route('/home', name: 'app_home')]
-    public function indeă(): Response
+    public function index(): Response
     {
-        return $this->render('index.html.twig', [
+        return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
         ]);
     }
